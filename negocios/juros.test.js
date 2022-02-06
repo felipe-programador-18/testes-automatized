675c8435f51d9a832cabb02bd1b109395b5eb94c
FeProg-18 <79095518+felipe-programador-18@users.noreply.github.com>
2022-02-06 18:33:49 -0300

    c7- modify name test

---
+++ negocios/juros.test.js
@@ -0,0 +1,11 @@
+const juros = require('./juros')
+const expect = require('chai').expect
+
+
+describe('test module about invoices', () =>{
+   it('resolve code about', () => {
+       const Test1010 = juros.Caljuros(1000,0.01,1)
+       expect(Test1010).to.equal(1210)
+   })
+})
+
