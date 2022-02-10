f314e9ec27bdbcb1e8246f3cbe82ddbb2934e2e2
FeProg-18 <79095518+felipe-programador-18@users.noreply.github.com>
2022-02-10 19:28:19 -0300

    c8- testing code

--- negocios/juros.test.js
+++ negocios/juros.test.js
@@ -5,7 +5,7 @@ const expect = require('chai').expect
 describe('test module about invoices', () =>{
    it('resolve code about', () => {
        const Test1010 = juros.Caljuros(1000,0.01,1)
-       expect(Test1010).to.equal(1210)
+       expect(Test1010).to.equal(1010)
    })
 })
 
