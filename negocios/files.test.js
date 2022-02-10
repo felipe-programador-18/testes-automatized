949f1917001c465932b511b1eca65e6cbee5b675
FeProg-18 <79095518+felipe-programador-18@users.noreply.github.com>
2022-02-10 19:29:48 -0300

    c11- testing code all

---
+++ negocios/files.test.js
@@ -0,0 +1,18 @@
+const files = require('./files')
+//when i build one const expect i have get require. expect of chai too.
+const expect = require('chai').expect
+const fs = require('fs')
+const sinon = require('sinon')
+
+
+describe('test directory ', () => {
+    it('reads a directory', () => {
+        sinon.stub(fs, 'readdir').callsFake((path, cb) =>{
+         cb(null, ['file1.txt'], path)
+        })
+      
+        return files.readdir('./').then( list => {
+          console.log(list)
+      })
+    })
+} )
